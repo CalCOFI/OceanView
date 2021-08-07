27e8dca9f0e8cad5ea00eb506be2343cbd9dd91e
hundredball <chh008@eng.ucsd.edu>
2021-08-06 22:49:58 -0700

    Working on iOS version

---
+++ android/app/src/main/kotlin/edu/ucsd/h4oceans/ecoapp/oceanview/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.ucsd.h4oceans.ecoapp.oceanview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,12 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
     GeneratedPluginRegistrant.register(with: self)
+    // Firebase
+    FirebaseApp.configure()
+    // GoogleMaps
+    GMSServices.provideAPIKey("AIzaSyBpDLenOUhz8cWFT0Oc7gWD4MneSUboyVc")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
