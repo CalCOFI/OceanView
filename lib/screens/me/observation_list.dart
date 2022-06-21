11834518573e47466381c22e3569256cb5159786
hundredball <chh008@eng.ucsd.edu>
2022-06-20 23:10:47 -0700

    Add customScrollView for MePage

--- lib/screens/me/observation_list.dart
+++ lib/screens/me/observation_list.dart
@@ -1,8 +1,12 @@
 
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:image_stack/image_stack.dart';
+
 import 'package:ocean_view/models/observation.dart';
 import 'package:ocean_view/screens/me/me_observation.dart';
-import 'package:provider/provider.dart';
+
 
 /*
   A page showing user's past observations with only images organized
@@ -21,43 +25,99 @@ class _ObservationListState extends State<ObservationList> {
     //and stored the observation data in observations
     final observations = Provider.of<List<Observation>?>(context) ?? [];
 
+    // CustomScrollView
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      // A child Widget that show the images in a grid view
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        itemCount: observations.length,
-        itemBuilder: (BuildContext ctx, index) {
-          return Container(
-              alignment: Alignment.center,
-              //A widget that contains the onTap() function which passes the observation
-              //data to MeObservation() when clicking an image
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          MeObservation(observation: observations[index]),
-                      settings: RouteSettings(
-                        arguments: observations[index],
-                      ),
-                    ),
-                  );
-                },
-                //Image Widget which displays the image
-                child: Image(
-                  image: NetworkImage(observations[index].url!),
-                  height: 250,
-                  width: 180,
-                ),
-              )
-          );
-        },
+      padding: const EdgeInsets.all(10.0),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('Single Observation')
+          ),
+          const SliverPadding(padding: EdgeInsets.symmetric(vertical: 5)),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 1,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (cxt, index) {
+                return Container(
+                    alignment: Alignment.center,
+                    //A widget that contains the onTap() function which passes the observation
+                    //data to MeObservation() when clicking an image
+                    child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  MeObservation(observation: observations[index]),
+                              settings: RouteSettings(
+                                arguments: observations[index],
+                              ),
+                            ),
+                          );
+                        },
+                        //Image Widget which displays the image
+                        child: Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(observations[index].url!),
+                                )
+                            )
+                        )
+                    )
+                );
+              },
+              childCount: observations.length,
+            ),
+          ),
+          const SliverPadding(padding: EdgeInsets.symmetric(vertical: 5)),
+          SliverAppBar(
+            title: Text('Collections')
+          ),
+          const SliverPadding(padding: EdgeInsets.symmetric(vertical: 5)),
+          // TODO: Add image stack for collections
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (cxt, index) {
+                List<String> images = [];
+                for (Observation observation in observations) {
+                  images.add(observation.url!);
+                };
+                return Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // TODO: Add date and lasted time to this text
+                        Text('Collection $index'),
+                        // TODO: Only access collection here
+                        GestureDetector(
+                            onTap: () {
+                              print('Press $index stack');
+                            },
+                            //Image Widget which displays the image
+                            child: ImageStack(
+                              imageList: images,
+                              totalCount: observations.length,
+                              imageRadius: 50,
+                              imageCount: 3,
+                              imageBorderWidth: 3,
+                            )
+                        ),
+                      ],
+                    )
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+          const SliverPadding(padding: EdgeInsets.symmetric(vertical: 5)),
+        ],
       ),
     );
   }
