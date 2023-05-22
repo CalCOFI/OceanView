95ada40019411ce57153d1ce242a66bbe366f54e
hundredball <chh008@eng.ucsd.edu>
2023-05-22 00:14:45 -0700

    Change dummy_test to follow dart format

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Dummy testing', () {
     test('1 == 1', () {
-      expect(1==1, true);
+      expect(1 == 1, true);
     });
   });
 }
